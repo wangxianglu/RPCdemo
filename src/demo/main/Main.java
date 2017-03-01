509d3036a5d20fe94d8b6d6d3da39b5024c45b76
wangxianglu <wangxianglu@dianmei.com>
2017-03-01 14:24:33 +0800

    1111

--- src/demo/main/Main.java
+++ src/demo/main/Main.java
@@ -13,19 +13,21 @@ import java.net.InetSocketAddress;
 public class Main {
 
     public static void main(String[] args) throws Exception{
+        // 创建一个异步 发布服务端的线程并 启动
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    RpcExporter.exporter("localhost", 8080);
+                    RpcExporter.exporter("localhost", 8888);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }).start();
 
+        // 发起RPC 客户端请求
         RpcImporter<EchoService> importer = new RpcImporter<EchoService>();
-        EchoService echoService = importer.importer(EchoServiceImpl.class, new InetSocketAddress("localhost", 8080));
+        EchoService echoService = importer.importer(EchoServiceImpl.class, new InetSocketAddress("localhost", 8888));
         System.out.print(echoService.echo("Are ou ok?"));
     }
 }
